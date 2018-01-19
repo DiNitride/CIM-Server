2eb014ff2cb2d936f01b4e6b9d4e4083a10e744d
DiNitride <jamesthecookie@gmail.com>
2018-01-19 16:42:11 +0000

    Updated Log File Format

--- CIM/__init__.py
+++ CIM/__init__.py
@@ -22,14 +22,8 @@ logger.setLevel(logging.DEBUG)
 
 log_format = logging.Formatter('%(asctime)s - %(levelname)8s [%(name)s] : %(message)s')
 
-
-file_handler = logging.FileHandler(filename=f"{os.getenv('CIM_ROOT')}/logs/{t}", encoding="utf-8", mode="w")
+file_handler = logging.FileHandler(filename=f"{os.getenv('CIM_ROOT')}/logs/{t}.log", encoding="utf-8", mode="w")
 file_handler.setFormatter(log_format)
-
-# stream_handler = logging.StreamHandler()
-# stream_handler.setFormatter(log_format)
-
 logger.addHandler(file_handler)
-# logger.addHandler(stream_handler)
 
 logger.info("Logger Initialised")
